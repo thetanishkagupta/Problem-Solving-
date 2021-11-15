12d66ac57679d7a9a90b2475f315288da2a4a17b
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2021-11-15 22:14:11 +0530

    Update find the maximum and minimum element in array.py

--- find the maximum and minimum element in array.py
+++ find the maximum and minimum element in array.py
@@ -1,4 +1,24 @@
-N = int(input("Enter number of elements: "))
-Arr = list(map(int,input("Enter the elements: ").strip().split()))[:N]
-print("The minimum element is", min(Arr))
-print("The maximum element is", max(Arr))        
+n = int(input("Enter number of elements: "))
+arr = list(map(int,input("Enter the elements: ").strip().split()))[:n]
+if n==1:
+    max = arr[0]
+    min = arr[0]
+    
+if arr[0] > arr[1]:
+    max = arr[0]
+    min = arr[2]
+else:
+    max = arr[1]
+    min = arr[0]
+    
+for i in range(2, n):
+        if arr[i] > max:
+            max = arr[i]
+        elif arr[i] < min:
+            min = arr[i]
+ 
+print("maximum element: ",max)
+print("minimum element: ",min)
+    
+    
+    
