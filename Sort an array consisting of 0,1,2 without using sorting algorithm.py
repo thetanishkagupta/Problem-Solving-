db3b31fad5448849046a69930b1b726165053f23
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2021-11-16 18:46:39 +0530

    Create Sort an array consisting of 0,1,2 without using sorting algorithm.py

---
+++ Sort an array consisting of 0,1,2 without using sorting algorithm.py
@@ -0,0 +1,25 @@
+n = int(input("Enter number of elements: "))
+arr = list(map(int,input("Enter the elements: ").strip().split()))[:n]
+c0 = 0
+c1 = 0
+c2 = 0
+for i in range(n):
+    if arr[i] == 0:
+        c0 += 1
+    if arr[i] == 1:
+        c1 += 1
+    if arr[i] == 2:
+        c2 += 1
+        
+k = 0
+for i in range(c0):
+    arr[k] = 0
+    k += 1
+for i in range(c1):
+    arr[k] = 1
+    k += 1
+for i in range(c2):
+    arr[k] = 2
+    k += 1
+        
+print(arr)
