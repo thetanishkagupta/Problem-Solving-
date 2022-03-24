934a6ead3a3568bfd1718e68b977ec83ee7f3b86
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2022-03-24 11:18:00 +0530

    Update Create Next Greater Element to the left.py

--- Create Next Greater Element to the left.py
+++ Create Next Greater Element to the left.py
@@ -1,16 +1,27 @@
-class Solution:
-    def nextLargerElement(self,arr,n):
-        stack = []
-        result = [] #array for storing result
-        for i in range(0,n): # traversing the array from left to right
-            # to fetch the top element of stack from a list or array use stack[-1](for peek and pop)
-            while (len(stack) !=0 and stack[-1] <= arr[i]):
-                stack.pop()
-                
-            if len(stack) == 0: # if stack is empty
-                result.append(-1)  # put -1 in result array
-            else:
-                result.append(stack[-1])  # else pop the top element
-            stack.append(arr[i])
-            
-        return result
+from collections import deque
+def nearestGreaterToLeft(arr, n):
+    stack = deque()
+    ans = []
+    for i in range(0,n,1):
+        while (len(stack) != 0 and stack[-1] <= arr[i]):
+            stack.pop()
+        if len(stack) == 0:
+            ans.append(-1)
+        else:
+            ans.append(stack[-1])
+        stack.append(arr[i])
+    return ans
+n = int(input())
+arr = list(map(int,input().split()))
+ans = nearestGreaterToLeft(arr,n)
+print(*ans)
+
+
+'''
+Input: -
+5
+24 11 13 21 3
+Output:-
+-1 24 24 24 21
+Time Complexity: O(N) 
+'''
