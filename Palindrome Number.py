192d96a4e82a91b80ed071f04a5eac1f803b6466
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2022-04-30 13:04:34 +0530

    Update Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -12,3 +12,18 @@ class Solution(object):
             return True
         else:
             return False
+
+        
+
+# Another method
+class Solution(object):
+    def isPalindrome(self, i):
+        rev = 0
+        x = i
+        while(i>0):
+            rev = (rev * 10) + i % 10
+            i = i // 10
+        if x == rev:
+            return True
+        else:
+            return False
