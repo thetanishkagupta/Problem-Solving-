67c128692e94c72f1e9565b5ae7026e0e4d67124
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2022-05-09 16:02:39 +0530

    Update Fibonacci Number.py

--- Fibonacci Number.py
+++ Fibonacci Number.py
@@ -18,4 +18,36 @@ class Solution:
         for i in range(n):
             ans.append(ans[i] + ans[i+1])
         return ans[n] % 1000000007
-       
+  
+
+# Other Method
+def fib(n):
+    a = 0 
+    b = 1 
+    if n == 1:
+        print(a)
+    else:
+        print(a)
+        print(b)
+        
+        for i in range(2,n):
+            c = a + b 
+            a = b 
+            b = c 
+            print(c)
+n = int(input())
+fib(n)
+            
+    
+# Other method    
+def fib(i):
+    if i == 0:
+        return 0 
+    elif i == 1 :
+        return 1 
+    else:
+        return fib(i-2)+fib(i-1)
+
+n = int(input())
+for x in range(n):
+    print(fib(x))
